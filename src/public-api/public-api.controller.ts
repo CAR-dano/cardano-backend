9578f2d64fc83ef2c4ea1fac46342ed13ff8a21f
Maulana-anjari <maulana17anjari@gmail.com>
2025-06-30 15:20:12 +0700

    feat: add 2 endpoints to get inspections based on ID and change history without authentication for PDF generator requirements.

--- src/public-api/public-api.controller.ts
+++ src/public-api/public-api.controller.ts
@@ -17,23 +17,29 @@ import {
   HttpStatus,
   Logger,
   InternalServerErrorException,
+  Param,
 } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
-import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 import { UserResponseDto } from '../users/dto/user-response.dto';
 import { LatestArchivedInspectionResponseDto } from 'src/inspections/dto/latest-archived-inspection-response.dto';
 import { InspectionsService } from 'src/inspections/inspections.service';
+import { PublicApiService } from './public-api.service';
+import { InspectionResponseDto } from 'src/inspections/dto/inspection-response.dto';
+import { InspectionChangeLog } from '@prisma/client';
+import { InspectionChangeLogResponseDto } from 'src/inspection-change-log/dto/inspection-change-log-response.dto';
 
 @ApiTags('Public Users') // Tag for Swagger documentation
 @Controller('public') // Base path for this controller
-export class PublicUsersController {
-  private readonly logger = new Logger(PublicUsersController.name);
+export class PublicApiController {
+  private readonly logger = new Logger(PublicApiController.name);
 
   constructor(
     private readonly usersService: UsersService,
     private readonly inspectionsService: InspectionsService,
+    private readonly publicApiService: PublicApiService,
   ) {
-    this.logger.log('PublicUsersController initialized');
+    this.logger.log('PublicApiController initialized');
   }
 
   /**
@@ -105,5 +111,62 @@ export class PublicUsersController {
         );
       }
     });
+  }
+  @Get('inspections/:id')
+  @ApiOperation({
+    summary: 'Retrieve a specific inspection by ID',
+    description:
+      'Retrieves a specific inspection by ID, applying role-based visibility rules.',
+  })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    format: 'uuid',
+    description: 'The UUID of the inspection to retrieve.',
+  })
+  @ApiResponse({
+    status: 200,
+    description: 'The inspection record summary.',
+    type: InspectionResponseDto,
+  })
+  @ApiResponse({ status: 404, description: 'Inspection not found.' })
+  async findOne(@Param('id') id: string): Promise<InspectionResponseDto> {
+    const inspection = await this.publicApiService.findOne(id);
+    return new InspectionResponseDto(inspection);
+  }
+
+  /**
+   * Retrieves change logs for a specific inspection.
+   * Restricted to ADMIN and REVIEWER roles only.
+   *
+   * @param inspectionId The ID of the inspection.
+   * @returns A promise that resolves to an array of InspectionChangeLog objects.
+   * @throws UnauthorizedException if the user is not authenticated.
+   * @throws ForbiddenException if the user does not have the required role.
+   * @throws NotFoundException if the inspection is not found.
+   */
+  @Get('inspections/:id/changelog')
+  @ApiOperation({
+    summary: 'Get inspection change log',
+    description: 'Retrieves the change log entries for a specific inspection.',
+  })
+  @ApiParam({
+    name: 'id',
+    type: String,
+    description: 'The ID of the inspection',
+  })
+  @ApiResponse({
+    status: HttpStatus.OK,
+    description: 'Successfully retrieved inspection change log.',
+    type: [InspectionChangeLogResponseDto],
+  })
+  @ApiResponse({
+    status: HttpStatus.NOT_FOUND,
+    description: 'Inspection not found.',
+  })
+  async findChangesByInspectionId(
+    @Param('id') inspectionId: string,
+  ): Promise<InspectionChangeLog[]> {
+    return this.publicApiService.findChangesByInspectionId(inspectionId);
   }
 }
