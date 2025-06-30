f639b34b2736b151c15878d23cfd83b24bbb99e9
Maulana Anjari Anggorokasih <maulana17anjari@gmail.com>
2025-06-30 15:21:38 +0700

    feat: add 2 endpoints to get inspections based on ID and change history without authentication for PDF generator requirements. (#33)

--- src/public-api/public-api.module.ts
+++ src/public-api/public-api.module.ts
@@ -6,17 +6,18 @@
  * --------------------------------------------------------------------------
  * Description: NestJS module for public API endpoints.
  * Imports the UsersModule to access user-related data and services.
- * Declares the PublicUsersController to handle public user-related routes.
+ * Declares the PublicApiController to handle public user-related routes.
  * --------------------------------------------------------------------------
  */
 import { Module } from '@nestjs/common';
-import { PublicUsersController } from './public-users.controller';
+import { PublicApiController } from './public-api.controller';
+import { PublicApiService } from './public-api.service';
 import { UsersModule } from '../users/users.module';
 import { InspectionsModule } from '../inspections/inspections.module';
 
 @Module({
   imports: [UsersModule, InspectionsModule],
-  controllers: [PublicUsersController],
-  providers: [],
+  controllers: [PublicApiController],
+  providers: [PublicApiService],
 })
 export class PublicApiModule {}
